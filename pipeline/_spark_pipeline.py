e18974bca70b71a4ef251a392a5bd925c1b9c5b7
Jaime Pons <55850388+Jaime47@users.noreply.github.com>
2024-03-10 11:03:55 +0100

    Submission function

--- pipeline/_spark_pipeline.py
+++ pipeline/_spark_pipeline.py
@@ -28,17 +28,16 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 # Database path
 DATABASE_PATH = "./database/DDBB_duckdb.duckdb"
 
-def movies_preprocessing(movies: pd.DataFrame) -> pd.DataFrame:
+def input_preprocessing(input_df: pd.DataFrame) -> pd.DataFrame:
     """
     Preprocessing actions required for the movies dataset.
     
     :param movies: pd.DataFrame containing the movies table from DuckDB
     """
     # Temporal fix: There are NaN columns that break the cast from Pandas Dataframe to Spark Dataframe in main().
-    movies.dropna(axis=0, inplace=True)
-    movies.loc[:, 'label'].replace({True: 'True', False: 'False'}, inplace=True)
-    return movies
-    
+    input_df.dropna(axis=0, inplace=True)
+    input_df.loc[:, 'label'].replace({True: 'True', False: 'False'}, inplace=True)
+    return input_df
 
 def fetch_duckdb() -> list[pd.DataFrame]:
     """
@@ -50,7 +49,7 @@ def fetch_duckdb() -> list[pd.DataFrame]:
     con = duckdb.connect(database=DATABASE_PATH, read_only=False)
     movies = con.execute('''select * from movies''').fetch_df()
     con.close()
-    movies = movies_preprocessing(movies)
+    movies = input_preprocessing(movies)
 
     return [movies]
 
@@ -62,7 +61,7 @@ def feature_selection(data: list[pd.DataFrame]):
     :param list[pd.DataFrame] Containing all the required datasets (movies and writers)
     """
 
-    return [["title_length", "title_changed"]]
+    return [["num_votes"]]
 
 def generate_pipeline(features: list) -> Pipeline:
     """
@@ -78,6 +77,64 @@ def generate_pipeline(features: list) -> Pipeline:
     pipeline = Pipeline().setStages([assembler, indexer])
     return pipeline
 
+def generate_output_pipeline(features: list) -> Pipeline:
+    """
+    Function to generate the Spark pipeline based on the following operations:
+        - Assembling (choosing) the desired features (numeric).
+        - Index the selected features to be processed by the pipeline (strings).
+        - Initializing the pipeline based on the indexed features.
+    
+    :param
+    """
+    assembler = VectorAssembler(inputCols=features[0], outputCol="features")
+    pipeline = Pipeline().setStages([assembler])
+    return pipeline
+
+def read_validation() -> pd.DataFrame:
+    """
+    Reads the validation dataset from its .csv file
+    
+    :param
+    """
+    validation = pd.read_csv(r"data\validation_hidden.csv")
+    return validation
+    
+def read_test() -> pd.DataFrame:
+    """
+    Reads the test dataset from its .csv file
+    
+    :param
+    """
+    test = pd.read_csv(r"data\test_hidden.csv")
+    return test
+    
+def create_submission(model, validation, test) -> None:
+    """
+    Create the required submission file in .csv format
+    
+    :param model: PySpark generated binary classifier
+    """    
+    pipeline = generate_output_pipeline([['numVotes']])
+    pipeline_fit = pipeline.fit(validation)
+    p_val = pipeline_fit.transform(validation)
+    p_test = pipeline_fit.transform(test)
+    
+    validation = validation.toPandas()  
+    test = test.toPandas()  
+    
+    validation["label"] = model.transform(p_val).select('prediction').collect().tolist
+    test["label"] = model.transform(p_test).select('prediction').collect().tolist
+
+    # Cast to bool and store in .csv
+    validation["label"].astype(bool).to_csv("val_result.csv", index=False, header=None)
+    test["label"].astype(bool).to_csv("test_result.csv", index=False, header=None)
+
+    # Generate final submission
+    for file in ["val_result.csv", "test_result.csv"]:
+        with open(file, 'r+') as f:
+            f.seek(0,2)                    
+            size=f.tell()               
+            f.truncate(size-2)
     
 def main() -> None:
     """
@@ -99,15 +156,22 @@ def main() -> None:
     pipeline = generate_pipeline(features)
     
     # Fit the pipeline using the Spark Dataframe
-    model = pipeline.fit(df_spark)  
+    pipeline_fit = pipeline.fit(df_spark)  
     
     # Generate and train the model
-    prepared = model.transform(df_spark)
-    dt = DecisionTreeClassifier(labelCol = "label-index", featuresCol="features")
+    prepared = pipeline_fit.transform(df_spark)
+    dt = DecisionTreeClassifier(labelCol = "label-index", featuresCol= "features")
     dt_model = dt.fit(prepared)
     
-    # Print some results
-    print(dt_model.transform(prepared).select("prediction", "label-index").take(10))
+    # Read output generation files
+    validation = read_validation()
+    test = read_test()
+    
+    df_validation = sqlContext.createDataFrame(validation)
+    df_test = sqlContext.createDataFrame(test)
+
+    create_submission(dt_model, df_validation, df_test)
+    
 
 
 if __name__ == '__main__':
